b3019796b36553b5723250c0f231390758d31cce
Emily Woods <emilymonica.woods@gmail.com>
2019-09-01 17:52:16 +0200

    complete rough working draft of weekly cron

--- cron/weekly.py
+++ cron/weekly.py
@@ -8,7 +8,8 @@ from copy import deepcopy
 from io import open
 from json import loads
 from typing import List, Dict
-from crate.client import connect
+
+from crate import client
 from crate.client.cursor import Cursor
 
 import requests
@@ -16,7 +17,7 @@ import requests
 
 def parse_args(args) -> configparser.ConfigParser:
     parser = argparse.ArgumentParser()
-    parser.add_argument("-c", "--config", dest='config_file', required=True, type=str)
+    parser.add_argument("-c", "--config", dest="config_file", required=True, type=str)
     args = parser.parse_args(args)
 
     config_file = args.config_file
@@ -26,9 +27,27 @@ def parse_args(args) -> configparser.ConfigParser:
     return config
 
 
+def create_table(cursor: Cursor):
+    import pdb
+
+    pdb.set_trace()
+
+    cursor.execute(
+        """CREATE TABLE IF NOT EXISTS tasks (
+        ts_completed TIMESTAMP,
+        description TEXT,
+        name STRING,
+        ts_due TIMESTAMP,
+        labels ARRAY(string))
+        """
+    )
+
+
 def get_list_of_cards(board_id, key, token) -> List:
-    url_to_list_cards = f"https://api.trello.com/1/boards/{board_id}/cards?key={key}&token={token}"
-    response = requests.get(url=url_to_list_cards) # add try/catch
+    url_to_list_cards = (
+        f"https://api.trello.com/1/boards/{board_id}/cards?key={key}&token={token}"
+    )
+    response = requests.get(url=url_to_list_cards)  # add try/catch
     return loads(response.text)
 
 
@@ -61,10 +80,10 @@ def extract_data(task: Dict):
     """
     return {
         "ts_completed": task["dateLastActivity"],
-        'description': task["desc"],
-        'name': task["name"],
-        'ts_due': task["due"],
-        "labels": [label["name"] for label in task["labels"]]
+        "description": task["desc"],
+        "name": task["name"],
+        "ts_due": task["due"],
+        "labels": [label["name"] for label in task["labels"]],
     }
 
 
@@ -80,12 +99,12 @@ def get_task_distribution(filtered_tasks: List) -> Dict:
 
 
 def get_labels(tasks: List) -> List:
-    label_list_of_lists = [task['labels'] for task in tasks]
+    label_list_of_lists = [task["labels"] for task in tasks]
     return list(itertools.chain(*label_list_of_lists))
 
 
 def create_weekly_summary_report(num_tasks_completed, distribution, tasks):
-    f=open("weekly-report.txt", "a+")
+    f = open("weekly-report.txt", "a+")
 
     intro = f"""
 Date: {time.time()}
@@ -103,22 +122,22 @@ The distribution of these tasks is broken down as {distribution}
     f.close()
 
 
-def insert_to_db(input: List):
+def insert_to_db(cursor: Cursor, input: List):
     raw = [tuple(record.values()) for record in input]
+    import pdb
 
-    with connect("http://localhost:4200/", username="", password="") as conn:
-        cursor: Cursor = conn.cursor()
-        cursor.executemany(
-            "INSERT INTO tasks (ts_completed, description, name, ts_due, labels) "
-            "VALUES (?, ?, ?, ?, ?) ",
-            raw # list of tuples
-
-        ) # batch insert
-        cursor.close()
+    pdb.set_trace()
+    cursor.executemany(
+        "INSERT INTO tasks (ts_completed, description, name, ts_due, labels) "
+        "VALUES (?, ?, ?, ?, ?) ",
+        raw,  # list of tuples
+    )
 
 
 def archive_completed_tasks():
-    pass
+    requests.post(
+        url=f"https://api.trello.com/1/lists/{list_id}/archiveAllCards?key={key}&token={token}"
+    )
 
 
 def main(args) -> None:
@@ -128,9 +147,22 @@ def main(args) -> None:
     token = config.get("default", "token")
     list_id = config.get("default", "list_id")
 
+    conn = client.connect(
+        "http://localhost:4200",
+        username="crate",
+        password="",
+        timeout=10,
+        verify_ssl_cert=False,
+    )
+
+    cursor: Cursor = conn.cursor()
+
+    create_table(cursor)
     list_of_cards = get_list_of_cards(board_id, key, token)
 
-    completed_tasks = list(filter(lambda task: task['idList'] == list_id, list_of_cards))
+    completed_tasks = list(
+        filter(lambda task: task["idList"] == list_id, list_of_cards)
+    )
     num_tasks_completed = len(completed_tasks)
     print(f"This week you completed {num_tasks_completed} tasks\n")
 
@@ -149,9 +181,14 @@ def main(args) -> None:
     weekly_summary["distribution"] = distribution
     print(weekly_summary)
 
-    insert_to_db(filtered_tasks)
+    insert_to_db(cursor=cursor, input=filtered_tasks)
+
     archive_completed_tasks()
 
+    conn.close()
+
+
+archive_completed_tasks()
 
 if __name__ == "__main__":
     sys.exit(main(sys.argv[1:]))
